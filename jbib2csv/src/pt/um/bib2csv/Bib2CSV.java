d55b7b13a4df6d279f5d7472637694fd576a9479
Tiago Veloso <ti.veloso@gmail.com>
2010-11-20 20:29:39 +0000

    Some refactoring. Added prepareCSV method.

--- jbib2csv/src/pt/um/bib2csv/Bib2CSV.java
+++ jbib2csv/src/pt/um/bib2csv/Bib2CSV.java
@@ -9,6 +9,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 import bibtex.dom.BibtexAbstractEntry;
@@ -23,8 +24,10 @@ public abstract class Bib2CSV {
 	private static BibtexFile bibfile;
 	private static final BibtexParser parser = new BibtexParser(true);
 	private static List<BibtexEntry> entries;
+	private static Set<String> keys = new TreeSet<String>();
 	private static Set<String> keywords = new TreeSet<String>();
 	private static Set<String> fields = new TreeSet<String>();
+	private static Map<String, ArrayList<String>> csvMap = new TreeMap<String, ArrayList<String>>();
 
 	public static void parseFile(Reader inputFile) throws ParseException,
 			IOException {
@@ -40,6 +43,43 @@ public abstract class Bib2CSV {
 			result.add(entry.getEntryKey());
 		}
 
+		keys.addAll(result);
+		return result;
+	}
+
+	public static Set<String> getFields() {
+		Set<String> result = new TreeSet<String>();
+
+		for (BibtexEntry entry : entries) {
+			result.addAll(entry.getFields().keySet());
+		}
+
+		fields.addAll(result);
+		return result;
+	}
+
+	public static Set<String> getFieldValuesByEntry(BibtexEntry entry,
+			String field, String sep) {
+		Set<String> result = new TreeSet<String>();
+
+		List<BibtexAbstractValue> values = entry.getFieldValuesAsList(field);
+
+		for (BibtexAbstractValue value : values) {
+
+			String fieldValue = value.toString();
+			fieldValue = fieldValue.substring(1, fieldValue.length() - 1);
+
+			if (fieldValue.indexOf(sep) >= 0) {
+				String[] fieldValues = fieldValue.split(sep);
+
+				for (String singleField : fieldValues) {
+					result.add(singleField.trim());
+				}
+			} else {
+				result.add(fieldValue);
+			}
+		}
+
 		return result;
 	}
 
@@ -47,37 +87,31 @@ public abstract class Bib2CSV {
 		Set<String> result = new TreeSet<String>();
 
 		for (BibtexEntry entry : entries) {
-			List<BibtexAbstractValue> values = entry.getFieldValuesAsList(field);
-			for (BibtexAbstractValue value : values) {
-
-				String fieldValue = value.toString();
-				fieldValue = fieldValue.substring(1, fieldValue.length() - 1);
-
-				if (fieldValue.indexOf(sep) >= 0) {
-					String[] fieldValues = fieldValue.split(sep);
-					
-					for (String singleField : fieldValues) {
-						result.add(singleField.trim());
-					}
-				} else {
-					result.add(fieldValue);
-				}
-			}
+			getFieldValuesByEntry(entry, field, sep);
 		}
 
 		keywords.addAll(result);
 		return result;
 	}
-	
-	public static Set<String> getFields(){
-		Set<String> result = new TreeSet<String>();
-		
+
+	public static void prepreCSV() {
+		ArrayList<String> connections = null;
+		Set<String> entryKeywords;
+
 		for (BibtexEntry entry : entries) {
-			result.addAll(entry.getFields().keySet());
+			for (String keyword : keywords) {
+				connections = new ArrayList<String>();
+				entryKeywords = getFieldValuesByEntry(entry, "keywords", ",");
+
+				if (entryKeywords.contains(keyword)) {
+					connections.add("1");
+				}else{
+					connections.add("0");
+				}				
+			}
+			
+			csvMap.put(entry.getEntryKey().toString(), connections);
 		}
-		
-		fields.addAll(result);
-		return result;
 	}
 
 	public static Set<String> getAttributes() {
